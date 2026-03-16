1d575bb1d2849949bec8ed538737dd1a6780490f
nonk123 <me@nonk.dev>
2026-03-16 08:17:01 +0300

    weirdass patch for `0xA000410`

--- bq_websocket_platform.c
+++ bq_websocket_platform.c
@@ -1154,6 +1154,8 @@ static bool tls_init_client(pt_tls *tls, os_socket s, const bqws_pt_connect_opts
 	tls->ssl = SSL_new(g_tls.client_ctx);
 	if (!tls->ssl) return false;
 
+	SSL_set_tlsext_host_name(tls->ssl, client_opts->host);
+
 	BIO *bio = BIO_new_socket((int)s, 0);
 	if (!bio) return false;
 
